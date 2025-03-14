43f597602feeb0373486d2d77c14e1b8c9c6be98
Muhammad Ahmad <ahmadnaeem6277@gmail.com>
2025-03-14 23:00:37 +0500

    css changes

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -25,6 +25,7 @@ const PageNotFound = () => {
             className="text-white text-center bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-1 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
           >
             <button
+              className="cursor-pointer"
               onClick={() => {
                 navigate(-1);
               }}
